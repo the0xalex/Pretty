eae9c952367d70c7c384a49f1dec7613df019733
Alex Moore <james.a.moore01@gmail.com>
2022-05-03 07:21:01 -0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
-swift build -c release -v -Xswiftc -Ounchecked -Xcc -O3 \
+swift build -c release -Xswiftc -Ounchecked -Xcc -O3 \
   && cp `swift build -c release --show-bin-path`/Pretty . \
   && mv Pretty pretty
